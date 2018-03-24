9f1874fbb5733440700bf903bc818d013409f74e
Jan Filipowski <jachuf@gmail.com>
2018-03-24 05:46:16 +0100

    Extract fallback account_id to method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,16 @@ require 'revenue_generator/account_repository'
 
 class WelcomeController < ApplicationController
   def show
-    cookies[:account_id] ||= SecureRandom.uuid
-    @account = account_repository.fetch_or_create(cookies[:account_id])
+    @account = account_repository.fetch_or_create(account_id)
   end
 
   private
 
   def account_repository
     @account_repository ||= RevenueGenerator::AccountRepository.new
+  end
+
+  def account_id
+    cookies[:account_id] ||= SecureRandom.uuid
   end
 end
